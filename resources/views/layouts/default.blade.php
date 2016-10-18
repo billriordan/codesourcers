ef900964cc4d16810e5bc2bcc2af78b645e55d58
Adrian Gracia <Shyscope@gmail.com>
2016-10-18 17:32:44 -0500

    login button route

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -53,10 +53,10 @@
                 <ul class="nav navbar-nav navbar-right">
                     <!-- Authentication Links -->
                     @if (Auth::guest())
-                        <li><a href="{{ url('/auth/login') }}">Login</a></li>
+                        <li><a href="{{ url('/login') }}">Login</a></li>
                     @else
                         <li class="dropdown">
-                            <a href="{{ url('/auth/logout') }}" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                            <a href="{{ url('/logout') }}" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
                                 {{ Auth::user()->name }} Logout
                             </a>
 

fec000084da01c2256344b780e8ed6f0294c0a99
jared engler <jared.lite@gmail.com>
2016-11-15 19:10:46 -0600

    added a good quote to the about page

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -12,13 +12,17 @@
     <h1>About Us<h1>
 </div>
 
+<p>We are a passionate group of individuals currently pursuing general computer science degrees at Illinois State University. We believe that the learning process is not only an individual effort, but a social one; and thus, this forum was born.</p>
+
+<p>Feel free to contact us with business inqueries via our respective social media accounts:</p>
+
 <div class="links">
     <ul>
-		<li><a href="https://github.com/lagooned">Jared Engler</a></li>
-		<li><a href="https://github.com/billriordan">Bill Riordan</a></li>
-		<li><a href="https://github.com/">Cody Huggins</a></li>
-		<li><a href="https://github.com/Ltadrian">Adrian Gracia</a></li>
-		<li><a href="https://github.com/">Danny Kay</a></li>
+		<li>Jared Engler - <a href="https://github.com/lagooned">github</a></li>
+		<li>Adrian Gracia - <a href="https://github.com/Ltadrian">github</a></li>
+		<li>Cody Huggins - <a href="https://github.com/chuggins02">github</a></li>
+		<li>Danny Kay - <a href="https://github.com/">github</a></li>
+		<li>Bill Riordan - <a href="https://github.com/billriordan">github</a></li>
 	</ul> 
 </div>
 @endsection

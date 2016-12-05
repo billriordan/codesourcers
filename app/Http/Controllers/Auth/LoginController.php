ad8f3ed055e13c316640c3b845e15e4fba4cfc0f
Adrian Gracia <Shyscope@gmail.com>
2016-12-04 22:05:42 -0600

    Small fixes with flash messages

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -65,6 +65,7 @@ class LoginController extends Controller
 
         // if the validator fails, redirect back to the form
         if ($validator->fails()) {
+
             return Redirect::to('login')
                 ->withErrors($validator) // send back all errors to the login form
                 ->withInput(Input::except('password')); // send back the input (not the password) so that we can repopulate the form
@@ -96,6 +97,7 @@ class LoginController extends Controller
             } else {
 
                 // validation not successful, send back to form
+                flash('Invalid Password/Email', 'danger');
                 return Redirect::to('login');
 
             }
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,13 +2,7 @@
 
 @section('content')
 <div class="container">
-    @if (session()->has('flash_notification.message'))
-        <div class="alert alert-{{ session('flash_notification.level') }}">
-            <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
 
-            {!! session('flash_notification.message') !!}
-        </div>
-    @endif
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
